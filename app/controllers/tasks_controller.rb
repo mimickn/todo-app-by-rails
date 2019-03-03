eeb8986b96430a356179ec8775701f182ec2061d
EC2 Default User <ec2-user@ip-172-31-37-9.ap-southeast-1.compute.internal>
2019-03-03 08:11:04 +0000

    CRUD実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,38 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
+  
+  def new
+  end
+  
+  def create
+    @task = Task.new(content: params[:content])
+    if @task.save
+      redirect_to("/tasks/index")
+    else
+      render("tasks/new")
+    end
+  end
+  
+  def delete
+    task = Task.find_by(id: params[:id])
+    task.destroy
+    redirect_to("/tasks/index")
+  end
+  
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+  
+  def update
+    @task = Task.find_by(id: params[:id])
+    @task.content = params[:content]
+    if @task.save
+      redirect_to("/tasks/index")
+    else
+      render("tasks/edit")
+    end
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'tasks#index'
+  get  'tasks/index' => 'tasks#index'
+  get  'tasks/new' => 'tasks#new'
+  post 'tasks/create' => 'tasks#create'
+  post 'tasks/:id/delete' => 'tasks#delete'
+  get  'tasks/:id/edit' => 'tasks#edit'
+  post 'tasks/:id/update' => 'tasks#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
